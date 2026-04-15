2b3a3ee78a83469aa0026d26cd3eca7ae7437879
Milagros-villalm <villalm1@tcnj.edu>
2026-04-14 20:09:31 -0400

    Implemented logic to translate logical address to physical address, implemented functions: checkPageTable(), checkTLB(), translateLogicalAddr()

--- main1.c
+++ main1.c
@@ -16,26 +16,37 @@
 #define NUM_BYTES 256
 
 typedef struct pageTableEntry{
-    int valid; /* Memory status */
+    int inMemory; /* Memory status */
     int frameNumber; /* Frame in physical memory */
 }pageTableEntry;
 
+int pageEntriesCount = 0;
 pageTableEntry pageTable[PAGE_SIZE]; /* Page Table */
 
 typedef struct tlbEntry{ 
     int pageNumber; /* Page number */
     int frameNumber; /* Frame number */
-    int valid; /* Usage Status */
+    int occupied; /* Frame status: occupied or not */
 }tlbEntry;
 
+int tlbEntriesCount = 0;
 tlbEntry TLB[TLB_SIZE]; /* TLB */
 
-char physicalMemory[NUM_FRAMES][NUM_BYTES]; /* Physical Memory */
+char physicalMemory[NUM_FRAMES][NUM_BYTES]; /* Physical Memory: 65,536 bytes */
 
-void insertIntoMemory(){
+void addTLBEntry(int pageNumber){
+
+    tlbEntriesCount++;
+}
+
+void addPageEntry(int pageNumber){
+    pageEntriesCount++;
+}
+
+int insertIntoMemory(int pageNumber){
     /*
     Ex: if logical address with page number 15 = page fault, read in page 15 from
-        Backing_store and sore it in a page frame in physical memory (page table & TLB updated)
+        Backing_store and store it in a page frame in physical memory (page table & TLB updated)
 
     BACKING_STORE:
         - treat as random-access file to randomly seek to certain positions for r
@@ -46,29 +57,51 @@ void insertIntoMemory(){
     3. Update TLB and page table
     */
 }
-void checkPageTable(){
-    /*
-    1. Check table for page Number 
-    2. extract the corresponding frame number from the page table
-            - Hit: frame number obtained
-                   frame number + offset = physical address
-            - Miss: Page fault occurs
-    4. 
-    */
 
-   insertIntoMemory();
+int checkPageTable(int pageNumber){
+    if(pageTable[pageNumber].inMemory){ /* Checks if loaded in memory */
+        /* Add to TLB table */
+        TLB[tlbEntriesCount].pageNumber = pageNumber;
+        TLB[tlbEntriesCount].frameNumber = pageTable[pageNumber].frameNumber;
+        TLB[tlbEntriesCount].occupied = 1;
+
+        /* Increase table entry count */
+        if(tlbEntriesCount >= 16) tlbEntriesCount = 0;
+        else tlbEntriesCount++;
+
+        return pageTable[pageNumber].frameNumber; /* Hit: return frame number */
+    }
+
+    return -1; /* Return -1 if page fault */
 }
 
-void checkTLB(){
-    /*
-    Consult TLB
-        - TLB hit: frame number is obtained from the TLB
-        - TLB Miss: consult page table 
-    */
-   checkPageTable();
+int checkTLB(int pageNumber){
+    /* Loop through TLB table for page number */
+    for(int i = 0; i < TLB_SIZE; i++){
+        if(TLB[i].occupied && TLB[i].pageNumber == pageNumber)
+            return TLB[i].frameNumber; /* TLB hit */
+    }
+
+   return -1; /* TLB Miss: check page table */
 }
+
+/* Translate logical address to physical address */
+void translateLogicalAddr(int logicalAddr, int pageNum, int offset){
+    int frame = checkTLB(pageNum); /* Check TLB table */
+    if(frame == -1) frame = checkPageTable(pageNum); /* Check page table */
+    if(frame == -1) frame = insertIntoMemory(pageNum); /* Page fault: */
+    int physicalAddress = (frame << 8) | offset; /* Calculate physical address */
+} 
 
 int main(int argc, char *arg[]){
+    /* Initialize page table */
+    for(int i = 0; i < TLB_SIZE; i++){
+        pageTable[i].inMemory = 0;
+        pageTable[i].frameNumber = -1;
+    }
+
+    /* Initialize TLB table */
+    for(int i = 0; i < TLB_SIZE; i++) TLB[i].occupied = 0;
 
     /* Check command line argument: */
     if (argc != 2){
@@ -99,6 +132,8 @@ int main(int argc, char *arg[]){
         int offset = logical_address & 0xFF; /* Page offset */
 
         printf("logical=%d page=%d offset=%d\n", logical_address, page, offset);
+
+        translateLogicalAddr(logical_address, page, offset); /* Translate logical to physical addr */
     }
 
     /* Close files: */
